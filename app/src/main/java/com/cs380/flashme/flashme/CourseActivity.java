ef0848c6061556654d4dca7609f0d263c59a6d66
Brennan <bbarni840@gmail.com>
2016-04-24 12:07:53 -0700

    Added basic UI framework for CourseUI page

---
+++ app/src/main/java/com/cs380/flashme/flashme/CourseActivity.java
@@ -0,0 +1,12 @@
+package com.cs380.flashme.flashme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
