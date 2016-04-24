fa3e31a3147ab06e8f888803a018c8644089b947
Brennan <bbarni840@gmail.com>
2016-04-24 12:43:01 -0700

    Added CourseActivity java methods. Should now change "Course ID Here" to subject name passed via intent from previous menu

--- app/src/main/java/com/cs380/flashme/flashme/CourseActivity.java
+++ app/src/main/java/com/cs380/flashme/flashme/CourseActivity.java
@@ -2,11 +2,27 @@ package com.cs380.flashme.flashme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.cs380.flashme.flashme.data.Course;
+import com.cs380.flashme.flashme.data.DBHelper;
+import com.cs380.flashme.flashme.data.IntentConstants;
 
 public class CourseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String subject = getIntent().getStringExtra(IntentConstants.SUBJECT_KEY);
+        String courseNum = getIntent().getStringExtra(IntentConstants.COURSE_NUM_KEY);
+
+        DBHelper dbHelper = DBHelper.getInstance(this);
+
+        Course course = dbHelper.getCourse(subject, Integer.parseInt(courseNum));
+
+        TextView textview = (TextView) findViewById(R.id.courseTitle);
+        textview.setText(subject);
+        // method used to test; should set to name of subject on activity instantiation
+
     }
 }
