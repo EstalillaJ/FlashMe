7270ce4af066d25b663ea87024aa365a568ba2ea
Brennan <bbarni840@gmail.com>
2016-04-24 13:22:59 -0700

    Worked on the CourseActivity java class, implementing Christian's method that should populate the list. Not tested; will keep working on it.

--- app/src/main/java/com/cs380/flashme/flashme/CourseActivity.java
+++ app/src/main/java/com/cs380/flashme/flashme/CourseActivity.java
@@ -2,6 +2,8 @@ package com.cs380.flashme.flashme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.cs380.flashme.flashme.data.Course;
@@ -22,6 +24,10 @@ public class CourseActivity extends AppCompatActivity {
 
         TextView textview = (TextView) findViewById(R.id.courseTitle);
         textview.setText(subject);
+
+        ListView courseList = (ListView) findViewById(R.id.courseListView);
+        ArrayAdapter subjectAdapter = new ArrayAdapter<Integer>(this, R.layout.subject_layout, courseList); // not entirely sure if implementation is sound; will iterate further
+        courseList.setAdapter(subjectAdapter);
         // method used to test; should set to name of subject on activity instantiation
 
     }
