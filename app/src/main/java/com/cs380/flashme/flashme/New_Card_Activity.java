537aa5a1ff7a1dd9f07fabc61a6aac89266ea92e
brickeyr <brickeyr@cwu.edu>
2016-04-26 08:18:02 -0700

    created new flashcard object in New_Card_Activity.java

--- app/src/main/java/com/cs380/flashme/flashme/New_Card_Activity.java
+++ app/src/main/java/com/cs380/flashme/flashme/New_Card_Activity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.cs380.flashme.flashme.data.Course;
+import com.cs380.flashme.flashme.data.DBConstants;
 import com.cs380.flashme.flashme.data.FlashCard;
 
 public class New_Card_Activity extends AppCompatActivity {
@@ -20,9 +22,14 @@ public class New_Card_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new__card_);
 
+        //if(getIntent())
+
         SUBJECT = (EditText) findViewById(R.id.Course_Subject_ID);
+
         COURSE_NUMBER = (EditText) findViewById(R.id.Course_Number_ID);
+
         FRONT_DESCRIPTION = (EditText) findViewById(R.id.Side_Front_Text);
+
         BACK_DESCRIPTION = (EditText) findViewById(R.id.Side_Back_Text);
 
     }
@@ -35,8 +42,8 @@ public class New_Card_Activity extends AppCompatActivity {
         String Front_Description = FRONT_DESCRIPTION.getText().toString();
         String Back_Description = BACK_DESCRIPTION.getText().toString();
 
+        FlashCard card = new FlashCard(this, Course_Subject, Course_Number, Front_Description, Back_Description, DBConstants.USER_MADE);
 
-        Intent intent = new Intent(this, MainActivity.class);
 
 
 
