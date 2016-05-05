89a6ecf845bef0e010b03cb58de05c19094c015e
EstalillaJ <joshtestalilla@gmail.com>
2016-05-05 15:56:47 -0700

    Added a swipe to delete on Course card list

--- app/src/main/java/com/cs380/flashme/flashme/CourseActivity.java
+++ app/src/main/java/com/cs380/flashme/flashme/CourseActivity.java
@@ -1,9 +1,11 @@
 package com.cs380.flashme.flashme;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -14,17 +16,22 @@ import com.cs380.flashme.flashme.data.DBHelper;
 import com.cs380.flashme.flashme.data.FlashCard;
 import com.cs380.flashme.flashme.data.IntentConstants;
 import com.nhaarman.listviewanimations.appearance.simple.AlphaInAnimationAdapter;
+import com.nhaarman.listviewanimations.itemmanipulation.DynamicListView;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.OnDismissCallback;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.SimpleSwipeUndoAdapter;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.TimedUndoAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CourseActivity extends AppCompatActivity {
-    private  String subject;
+    private String subject;
     private String courseNum;
     private Course course;
     private List<FlashCard> cards;
     private String courseTitle;
     private DBHelper dbHelper;
+    private ArrayList<String> cardFronts;
     private ArrayAdapter<String> cardListAdapter;
 
     @Override
@@ -46,33 +53,12 @@ public class CourseActivity extends AppCompatActivity {
         TextView textview = (TextView) findViewById(R.id.courseTitle);
         textview.setText(courseTitle);
 
-        //create a list containg the fronts of all cards in course
-        ArrayList<String> cardFronts = new ArrayList<>();
-        for (FlashCard card: cards)
-            cardFronts.add(card.getFront());
-        //use the cardFronts list to populate our listview in the ui
-        ListView cardListView = (ListView)   findViewById(R.id.cardList);
-        cardListAdapter = new ArrayAdapter<>(this, R.layout.subject_layout, cardFronts);
-        AlphaInAnimationAdapter animationAdapter = new AlphaInAnimationAdapter(cardListAdapter);
-        animationAdapter.setAbsListView(cardListView);
-        cardListView.setAdapter(animationAdapter);
-
-        //set on click listener for cardList
-        cardListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                FlashCard card = cards.get(position);
-                Intent intent = new Intent(getApplicationContext(), New_Card_Activity.class);
-                intent.putExtra(IntentConstants.COURSE_NUM_KEY, courseNum);
-                intent.putExtra(IntentConstants.SUBJECT_KEY, subject);
-                intent.putExtra(IntentConstants.EXISTING_CARD_KEY, true);
-                intent.putExtra(IntentConstants.CARD_ID_KEY, card.getId());
-                startActivity(intent);
-            }
-        });
+        setUpCardList();
 
 
     }
+
+
 
     public void onResume(){
         super.onResume();
@@ -108,5 +94,47 @@ public class CourseActivity extends AppCompatActivity {
 
     public void viewCourseStatistics(View v){
         //TODO launch stats acvtivity
+    }
+
+
+    public void setUpCardList(){
+        //create a list containing the fronts of all cards in course
+        cardFronts = new ArrayList<>();
+        for (FlashCard card: cards)
+            cardFronts.add(card.getFront());
+
+        //use the cardFronts list to populate our listview in the ui
+        DynamicListView cardListView = (DynamicListView)   findViewById(R.id.cardList);
+        cardListAdapter = new ArrayAdapter<>(this, R.layout.subject_layout, cardFronts);
+
+        //set on click listener for cardList
+        cardListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FlashCard card = cards.get(position);
+                Intent intent = new Intent(getApplicationContext(), New_Card_Activity.class);
+                intent.putExtra(IntentConstants.COURSE_NUM_KEY, courseNum);
+                intent.putExtra(IntentConstants.SUBJECT_KEY, subject);
+                intent.putExtra(IntentConstants.EXISTING_CARD_KEY, true);
+                intent.putExtra(IntentConstants.CARD_ID_KEY, card.getId());
+                startActivity(intent);
+            }
+        });
+
+        //Allow users to swipe and remove cards
+        SimpleSwipeUndoAdapter swipeUndoAdapter = new SimpleSwipeUndoAdapter(cardListAdapter, CourseActivity.this, new OnDismissCallback() {
+            @Override
+            public void onDismiss(@NonNull ViewGroup listView, @NonNull int[] reverseSortedPositions) {
+                for (int i: reverseSortedPositions) {
+                    cardFronts.remove(i);
+                    dbHelper.removeCard(cards.get(i));
+                }
+                cardListAdapter.notifyDataSetChanged();
+            }
+        });
+
+        swipeUndoAdapter.setAbsListView(cardListView);
+        cardListView.setAdapter(swipeUndoAdapter);
+        cardListView.enableSimpleSwipeUndo();
     }
 }
