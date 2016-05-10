b15b2957fdccfde703ceb8d3fa19b798c89d073b
Brennan <bbarni840@gmail.com>
2016-05-10 08:53:18 -0700

    Added currently defunct accuracy view; will be complete today

--- app/src/main/java/com/cs380/flashme/flashme/StatsActivity.java
+++ app/src/main/java/com/cs380/flashme/flashme/StatsActivity.java
@@ -2,19 +2,32 @@ package com.cs380.flashme.flashme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.cs380.flashme.flashme.data.Course;
 import com.cs380.flashme.flashme.data.DBHelper;
 import com.cs380.flashme.flashme.data.IntentConstants;
 
 public class StatsActivity extends AppCompatActivity {
+    private String subject;
+    private String courseNum;
+    private String courseTitle;
+    private String courseAccuracy;
+    private Course course;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_stats);
-        //DBHelper dbHelper = DBHelper.getInstance(this);
-        //Course course = dbHelper.getCourse(subject,coursenum);
+        setContentView(R.layout.activity_stats);
+        DBHelper dbHelper = DBHelper.getInstance(this);
+
+        subject = getIntent().getStringExtra(IntentConstants.SUBJECT_KEY);
+        courseNum =  getIntent().getStringExtra(IntentConstants.COURSE_NUM_KEY);
+
+        courseTitle = subject + " " + courseNum + " Statistics:";
+        TextView textView = (TextView) findViewById()
+
+
 
     }
 }
