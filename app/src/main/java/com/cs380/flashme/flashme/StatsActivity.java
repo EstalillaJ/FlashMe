825aefa98dba238292db3e45af1ca631795a7732
Brennan <bbarni840@gmail.com>
2016-05-10 08:25:44 -0700

    Added StatsActivity activity.

---
+++ app/src/main/java/com/cs380/flashme/flashme/StatsActivity.java
@@ -0,0 +1,20 @@
+package com.cs380.flashme.flashme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cs380.flashme.flashme.data.Course;
+import com.cs380.flashme.flashme.data.DBHelper;
+import com.cs380.flashme.flashme.data.IntentConstants;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_stats);
+        //DBHelper dbHelper = DBHelper.getInstance(this);
+        //Course course = dbHelper.getCourse(subject,coursenum);
+
+    }
+}
